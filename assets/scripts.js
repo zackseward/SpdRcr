72f6e4cf6d0e5fc824c13c818c97913a1487ac91
Zack Seward <zackseward@Zacks-MacBook-Air.local>
2015-11-10 20:16:15 -0500

    Sytled pages, did form

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,8 +1,23 @@
 $(document).ready(function() {
-  console.log("This works!");
-
-
-
-
-
+  var formData = getQueryStrings();
+  console.log(formData['car']);
+  console.log(formData['track']);
+  console.log(formData['full-name']);
+  console.log(formData['email-address']);
 });
+
+function getQueryStrings() {
+  var assoc  = {};
+  var decode = function (s) { return decodeURIComponent(s.replace(/\+/g, " ")); };
+  var queryString = location.search.substring(1);
+  var keyValues = queryString.split('&');
+
+  for(var i in keyValues) {
+    var key = keyValues[i].split('=');
+    if (key.length > 1) {
+      assoc[decode(key[0])] = decode(key[1]);
+    }
+  }
+
+  return assoc;
+}
